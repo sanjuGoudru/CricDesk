8aed9a1dd14fff48ef98e06fc006b8b8df89ac5f
sanjuGoudru <sanjugr7022@gmail.com>
2019-10-01 22:07:20 +0530

    Auto scaling done

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -4,10 +4,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
-
+import javafx.scene.layout.BorderPane;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.VBox;
@@ -27,10 +28,30 @@ public class MainController implements Initializable {
 	private ImageView main_logo;
 	@FXML
 	private ImageView main_logo2;
-
+	@FXML
+	private BorderPane mainPane;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		rightAnchor.heightProperty().addListener(new ChangeListener<Object>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
+				double height=(double) newValue;
+				//System.out.println(height);
+				main_logo.setFitHeight(height/1.3);
+				main_logo2.setFitHeight(height/3.5);
+			}
+		});
+		rightAnchor.widthProperty().addListener(new ChangeListener<Object>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
+				double width=(double) newValue;
+				//System.out.println(width);
+				main_logo.setFitWidth(width/1.3);
+				main_logo2.setFitWidth(width/3.6);
+			}
+		});
 	}
 
 	

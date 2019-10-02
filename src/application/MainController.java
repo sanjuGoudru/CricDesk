083c142d5a5c7dd66d3b8ae6a0311e6113265d64
sanjuGoudru <sanjugr7022@gmail.com>
2019-10-02 10:33:09 +0530

    Alignment of main page completed

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -2,13 +2,9 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.VBox;
@@ -23,36 +19,21 @@ public class MainController implements Initializable {
 	@FXML
 	private ImageView about;
 	@FXML
-	private AnchorPane rightAnchor;
+	private GridPane centerGridPane;
 	@FXML
 	private ImageView main_logo;
 	@FXML
 	private ImageView main_logo2;
 	@FXML
 	private BorderPane mainPane;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		rightAnchor.heightProperty().addListener(new ChangeListener<Object>() {
 
-			@Override
-			public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
-				double height=(double) newValue;
-				//System.out.println(height);
-				main_logo.setFitHeight(height/1.3);
-				main_logo2.setFitHeight(height/3.5);
-			}
-		});
-		rightAnchor.widthProperty().addListener(new ChangeListener<Object>() {
-
-			@Override
-			public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue) {
-				double width=(double) newValue;
-				//System.out.println(width);
-				main_logo.setFitWidth(width/1.3);
-				main_logo2.setFitWidth(width/3.6);
-			}
-		});
 	}
 
-	
+	public void mouseClick() {
+		System.out.println(main_logo2.getFitHeight() + "\t" + main_logo2.getFitWidth());// 130 150
+	}
+
 }

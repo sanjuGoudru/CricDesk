ae213611e808a22f570718caf94813ced1e0275a
18satwik <41579386+18satwik@users.noreply.github.com>
2019-10-11 21:58:49 +0530

    Tblleague created

---
+++ src/application/TblController.java
@@ -0,0 +1,38 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class TblController {
+	@FXML
+	private GridPane grid4;
+	@FXML
+	private Button dur;
+	@FXML
+	private Button ess;
+	@FXML
+	private Button glo;
+	@FXML
+	private Button hamp;
+	@FXML
+	private Button kent;
+	@FXML
+	private Button lanc;
+	@FXML
+	private Button leic;
+	@FXML
+	private Button mid;
+	@FXML
+	private Button som;
+	@FXML
+	private Button sur;
+	@FXML
+	private Button sus;
+	@FXML
+	private Button war;
+	@FXML
+	private Button worc;
+	@FXML
+	private Button york;
+}

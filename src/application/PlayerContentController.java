2c90bb8c2f63aad6189d4e81c224380d8f9c5544
sanjuGoudru <sanjugr7022@gmail.com>
2019-10-11 15:32:44 +0530

    asd

---
+++ src/application/PlayerContentController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+
+public class PlayerContentController {
+	@FXML
+	private Label name;
+	@FXML
+	private Label country;
+	@FXML
+	private Label role;
+
+}

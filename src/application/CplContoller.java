f2bcfd4fc6617720cb475499d157f3384d3b5663
18satwik <41579386+18satwik@users.noreply.github.com>
2019-10-12 17:38:36 +0530

    ClickMethod Added

--- src/application/CplContoller.java
+++ src/application/CplContoller.java
@@ -1,5 +1,12 @@
 package application;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import DAO.LeagueDAO;
+import Main.Player;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -8,15 +15,32 @@ public class CplContoller {
 	@FXML
 	private GridPane grid3;
 	@FXML
-	private Button jt;
+	private Button jt$2;
 	@FXML
-	private Button bt;
+	private Button bt$5;
 	@FXML
-	private Button stz;
+	private Button stz$6;
 	@FXML
-	private Button snp;
+	private Button snp$4;
 	@FXML
-	private Button tkr;
+	private Button tkr$1;
 	@FXML
-	private Button gaw;
+	private Button gaw$3;
+	public void click(ActionEvent e) {
+		Button b = (Button) e.getSource();
+		StringTokenizer st = new StringTokenizer(b.getId(), "$");
+		st.nextToken();
+		int tid = Integer.parseInt(st.nextToken());
+		LeagueDAO league = new LeagueDAO();
+		league.connect();
+		ArrayList<Player> players=null;
+		try {
+			players = league.getAllPlayers(tid, 3);
+			System.out.println(players);
+		} catch (SQLException e1) {
+			System.out.println("Failed to get players");
+			e1.printStackTrace();
+		}
+		
+	}
 }
--- src/application/TblController.java
+++ src/application/TblController.java
@@ -1,5 +1,12 @@
 package application;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import DAO.LeagueDAO;
+import Main.Player;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -8,31 +15,49 @@ public class TblController {
 	@FXML
 	private GridPane grid4;
 	@FXML
-	private Button dur;
+	private Button dur$7;
 	@FXML
-	private Button ess;
+	private Button ess$5;
 	@FXML
-	private Button glo;
+	private Button glo$2;
 	@FXML
-	private Button hamp;
+	private Button hamp$8;
 	@FXML
-	private Button kent;
+	private Button kent$11;
 	@FXML
-	private Button lanc;
+	private Button lanc$10;
 	@FXML
-	private Button leic;
+	private Button leic$4;
 	@FXML
-	private Button mid;
+	private Button mid$9;
 	@FXML
-	private Button som;
+	private Button som$1;
 	@FXML
-	private Button sur;
+	private Button sur$13;
 	@FXML
-	private Button sus;
+	private Button sus$12;
 	@FXML
-	private Button war;
+	private Button war$3;
 	@FXML
-	private Button worc;
+	private Button worc$14;
 	@FXML
-	private Button york;
+	private Button york$6;
+
+	public void click(ActionEvent e) {
+		Button b = (Button) e.getSource();
+		StringTokenizer st = new StringTokenizer(b.getId(), "$");
+		st.nextToken();
+		int tid = Integer.parseInt(st.nextToken());
+		LeagueDAO league = new LeagueDAO();
+		league.connect();
+		ArrayList<Player> players = null;
+		try {
+			players = league.getAllPlayers(tid, 4);
+			System.out.println(players);
+		} catch (SQLException e1) {
+			System.out.println("Failed to get players");
+			e1.printStackTrace();
+		}
+
+	}
 }

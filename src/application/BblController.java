a4158ab8028c4962c415decc252c660f3d1290db
18satwik <41579386+18satwik@users.noreply.github.com>
2019-10-12 17:13:33 +0530

    clickMethod added

--- src/application/BblController.java
+++ src/application/BblController.java
@@ -1,5 +1,12 @@
 package application;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import DAO.LeagueDAO;
+import Main.Player;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -8,19 +15,36 @@ public class BblController {
 	@FXML
 	private GridPane grid2;
 	@FXML
-	private Button ms;
+	private Button ms$3;
 	@FXML
-	private Button mr;
+	private Button mr$4;
 	@FXML
-	private Button sr;
+	private Button sr$2;
 	@FXML
-	private Button st;
+	private Button st$1;
 	@FXML
-	private Button bh;
+	private Button bh$6;
 	@FXML
-	private Button as;
+	private Button as$7;
 	@FXML
-	private Button ps;
+	private Button ps$5;
 	@FXML
-	private Button hh;
+	private Button hh$8;
+	public void click(ActionEvent e) {
+		Button b = (Button) e.getSource();
+		StringTokenizer st = new StringTokenizer(b.getId(), "$");
+		st.nextToken();
+		int tid = Integer.parseInt(st.nextToken());
+		LeagueDAO league = new LeagueDAO();
+		league.connect();
+		ArrayList<Player> players=null;
+		try {
+			players = league.getAllPlayers(tid, 2);
+			System.out.println(players);
+		} catch (SQLException e1) {
+			System.out.println("Failed to get players");
+			e1.printStackTrace();
+		}
+		
+	}
 }
